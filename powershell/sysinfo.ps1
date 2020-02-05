ea618e87aa4a1947d89cfd24e234f8d6a707486d
griffiwl <griffiwl@mail.uc.edu>
2020-02-04 19:01:18 -0500

    adding powershell files

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,16 @@
+﻿$Hello = "Hello, PowerShell"
+Write-Host($Hello)
+
+(Get-NetIPAddress).ipaddress
+
+(Get-NetIPAddress).ipv4address
+
+function getIP{
+(Get-NetIPAddress).ipv4address | Select-String "192*"
+}
+
+write-host(getIP)
+
+$IP = getIP
+Write-Host("This Machine's IP is $IP")
+Write-Host("This Machine's IP is {0}" -f $IP)
---
+++ powershell/sysinfoemailPS.ps1
@@ -0,0 +1,32 @@
+﻿function getIP{
+(Get-NetIPAddress).ipv4address | Select-String "192*"
+}
+
+
+function getcurrentDate{
+    Get-Date
+}
+
+
+function getComputerName{
+    $env:COMPUTERNAME
+}
+
+
+function getPSversion{
+    $Host.Version
+}
+
+
+function getUser{
+    $env:USERNAME
+}
+
+$IP = getIP
+$User = getUser
+$PSVersion = getPSversion
+$Hostname = getComputerName
+$Date = getcurrentDate
+
+Send-MailMessage -To "griffiwl@mail.uc.edu" -From "wadegriffith13@gmail.com" -Subject "IT3038c Windows SysInfo" -Body "This Machine's IP's:($IP) / The current user is:($User) / The PowerShell version is:($PSVersion) / The Hostname is:($Hostname) / The Date is:($Date)" -SmtpServer smtp.gmail.com -Port 587 -UseSSL -Credential (Get-Credential)
+ 
