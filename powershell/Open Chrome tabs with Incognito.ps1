2a506735d814c735181927e9d1d4a17476db6e5d
griffiwl <griffiwl@mail.uc.edu>
2020-02-06 17:40:28 -0500

    Project1 powershells scripts

---
+++ powershell/Open Chrome tabs with Incognito.ps1
@@ -0,0 +1,5 @@
+﻿#Opens each URL in a new tab with Incognito mode with Google Chrome.
+[system.Diagnostics.Process]::Start("chrome.exe","--incognito https://www.reddit.com/")
+[system.Diagnostics.Process]::Start("chrome.exe","--incognito https://www.plex.tv/")
+[system.Diagnostics.Process]::Start("chrome.exe","--incognito https://uc.instructure.com/")
+[system.Diagnostics.Process]::Start("chrome.exe", "--incognito https://www.youtube.com/")
