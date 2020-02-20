fd3c55d2fb506262f3ca2ed447d69e3ee69c2cc7
griffiwl <griffiwl@mail.uc.edu>
2020-02-20 10:57:36 -0500

    Finished server.js

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -3,6 +3,15 @@ var fs = require("fs");
 var os = require("os");
 var ip = require("ip");
 
+function serverUptime(){
+	uptime=os.uptime();
+	days = Math.floor(uptime / 86400);
+	hours = Math.floor(uptime % (86400) / 3600);
+	minutes = Math.floor(uptime % 3600 / 600);
+	seconds = Math.floor(uptime % 3600 % 60);
+return `Days:${days}, Hours:${hours}, Minutes:${minutes}, Seconds:${seconds}`;
+}
+
 http.createServer(function(req,res){
 
 	if(req.url==="/"){
@@ -13,6 +22,9 @@ res.end(body);
 }
 else if(req.url.match("/sysinfo")){
 myHostName=os.hostname();
+totalMemory =((os.totalmem())/1048576);
+freeMemory =((os.freemem())/1048576);
+cpuCount =os.cpus().length;
 html=`
 <!DOCTYPE html>
 <html>
@@ -22,10 +34,10 @@ html=`
 <body>
 <p>Hostname: ${myHostName}</p>
 <p>IP: ${ip.address()}</p>
-<p>Server Uptime:</p>
-<p>Total Memory:</p>
-<p>Free Memory:</p>
-<p>Number of CPUs:</p>
+<p>Server Uptime:${serverUptime()}</p>
+<p>Total Memory:${totalMemory} GB</p>
+<p>Free Memory:${freeMemory} GB</p>
+<p>Number of CPUs:${cpuCount}</p>
 </body>
 </html>
 `
@@ -33,7 +45,8 @@ res.writeHead(200,{"Content-Type":"text/html"});
 res.end(html);
 }
 else {
-res.writeHead('404 File Not Found at ${req.url}');
+res.writeHead(404, {"Content-Type":"text/plain"});
+res.end(`404 File Not Found at ${req.url}`);
 }
 
 
